41d77e29da175422b50734a2a4192baaba79ebfa
DylanBuys <buysdylan17@gmail.com>
2025-06-20 17:12:19 +0200

    Add files via upload

---
+++ detail.py
@@ -0,0 +1,9 @@
+#prompt user to input their name and age
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+#promt user to input their street name and house number
+street_name = input("Please enter your street name: ")
+house_number = input("Please enter your house number: ")
+#print the user's details
+print(f"Hello {name}, you are {age} years old.")
+print(f"You live at {house_number} {street_name}.")
