e7a49ddf37e51f7f4ad0eff29b6e1a46a3fd39e7
DylanBuys <buysdylan17@gmail.com>
2025-06-27 15:09:36 +0200

    Add files via upload

---
+++ list_types.py
@@ -0,0 +1,14 @@
+# store three names in a list
+friends_names = ['Jade Buys', 'Hagan Buys', 'Juandee Krieling']
+# print name of first friend
+print(friends_names[0])
+# print name of third friend
+print(friends_names[2])
+# print length of the list
+print(len(friends_names))
+
+friends_ages = [25, 30, 28]
+# print each friends name and age in a sentence
+print(f"{friends_names[0]} is {friends_ages[0]} years old.")
+print(f"{friends_names[1]} is {friends_ages[1]} years old.")
+print(f"{friends_names[2]} is {friends_ages[2]} years old.")
