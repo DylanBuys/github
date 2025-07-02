b6adbfecf0584d767a3714633445436e11ebdf09
DylanBuys <buysdylan17@gmail.com>
2025-07-02 18:36:16 +0200

    Add files via upload

---
+++ eroor2.py
@@ -0,0 +1,11 @@
+# This example program is meant to demonstrate errors.
+ 
+# There are some errors in this program. Run the program, look at the error messages, and find and fix the errors.
+
+animal = "Lion"  # syntax error fixed
+animal_type = "cub" # syntax error fixed
+number_of_teeth = 30  # syntax error fixed
+
+full_spec = f"This is a {animal}. It is a  {animal_type} and it has {number_of_teeth} teeth." # syntax error fixed #logical error fixed
+
+print(full_spec) # syntax error fixed
